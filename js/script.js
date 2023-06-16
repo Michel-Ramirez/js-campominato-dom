47fbf6803797183ed0793de96da917736e500c68
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-16 17:22:04 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -8,58 +8,93 @@ const level = document.getElementById('level');
 const scoreElement = document.getElementById('score');
 
 
-/**** | FUNCTIONS | *****/
-
-function createCell () {
-    const cell = document.createElement('div');
-    cell.classList.add('cell', 'level-10');
-
-    return cell;
-}
-
 
 
 
 //al click del bottone devo generare 100 celle
 button.addEventListener ('click', function (){
-
+    
+    /**** | FUNCTIONS | *****/
+    
+    function createCell () {
+        const cell = document.createElement('div');
+        cell.classList.add('cell', 'level-10');
+    
+        return cell;
+    }
+    
+    function getBomb (bombNumber, totalCells) {
+        let bombs = [];
+        
+        //estraggo numeri da pushare nel array fintanto che l'array non raggiunge la lunghezza del numero di bombe richieste 
+        while (bombs.length < bombNumber) {
+            let randomNumbers;
+            do {
+                //genero il numero random
+                randomNumbers = Math.floor(Math.random() * totalCells) + 1;
+                //fin quando il numero nel è presente nel array continuo ad estrarre
+            } while (bombs.includes(randomNumbers));
+            // pusho nel array
+            bombs.push(randomNumbers);
+        }
+        
+        return bombs;
+    }
+    
+    
     //cancello il contenuto del HTML in caso venga cliccato più volte il tasto play
     grid.innerHTML = ''
-
-
+    
     const totalCells = level.value * level.value
     
     //punteggio
     let score = 0;
+    scoreElement.innerText = score;
+    //numero bombe da posizionare
+    const bombNumber = 16;
     
+    //richiamo la funzione che mi restituisce l'array dei numeri random dove andranno posizionate le bombe
+    const bombs = getBomb(bombNumber, totalCells);
+    console.log(bombs)
+
     //Creo un ciclo per creare e stampare le celle nel dom
     for (let i = 0; i < totalCells; i++){
 
+        //richiamo la funzione che crea le celle
         const cell = createCell ();
 
         //stampo il nodo nel dom
         grid.appendChild(cell);
 
         // stampo i numeri da 1 a 100 centro le celle
-        const number = i + 1
-        cell.innerText = number
-        
+        const number = i + 1;
+        cell.innerText = number;
         
         // al click della cella aggiungo la classe
         cell.addEventListener('click', function(){
+            
+            //controllo se la la cella ha già la classe se non la ha la aggiungo altrimenti mi fermo
+            if(cell.classList.contains('clicked')) return;
+            
+            //aggiungo la classe alla casella cliccata
             cell.classList.add('clicked');
             console.log(number)
             
-            
-            for (i = 0; i < cell.length; i++){
-               
-                const clicked = document.querySelectorAll('.clicked');
-                score = clicked.length
-                console.log('FUNZIONA', score)
-                
-                scoreElement.innerText = clicked;
+            // se la bomba include il numero cliccato
+            const isBomb = bombs.includes(number);
+    
+            if (isBomb){
+                cell.classList.add('bomb');
+                setTimeout(() => {
+                    alert(`Hai erso totalizzando ${score}`);
+                }, 300);
+            } else {
+                //aumento il punteggio man mano che clicco sulle celle
+                score++
+                //scrivo il punteggio
+                scoreElement.innerText = score;
             }
-
+           
         });
 
     }
